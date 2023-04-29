3b22ea598ed6304d9ce91e4bf6d275b437b5086a
Tuba Bagriyanik <tubabagriyanik85@gmail.com>
2023-04-29 15:33:49 -0400

    son

--- src/Account.java
+++ src/Account.java
@@ -2,7 +2,9 @@ public class Account {
 
     public static void main(String[] args) {
         System.out.println("");
-        for (int i = 0; i < 10; i++) {
+       for(int i=0;i<10;i++){
         }
     }
 }
+
+//yeni commit
